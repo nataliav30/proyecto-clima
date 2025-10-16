52620c883b973c67e0c1682cfd07e08c435a384b
nataliav30 <nataliav86@gmail.com>
2025-10-16 16:01:30 +0200

    Added more JS funcionality via API

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ function refreshWeather(response){
 
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML= response.data.city;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+let windSpeedElement = document.querySelector("#wind-speed");
+windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+
+
 }
 
 function searchCity(city){
